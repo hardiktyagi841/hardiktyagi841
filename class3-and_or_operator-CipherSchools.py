d6e221bae1625b9788455ac2326da2dd25ab0f6f
hardiktyagi841 <119604493+hardiktyagi841@users.noreply.github.com>
2022-12-01 21:23:31 +0530

    Create class3-and_or_operator-CipherSchools.py

---
+++ class3-and_or_operator-CipherSchools.py
@@ -0,0 +1,31 @@
+# check two conditions at same time
+# and , or
+
+name = "abc"
+age = 19
+
+# And operator- Return True If all the conditions are satisfied.  
+if name == "abc" and age == 19:
+    print("Condition True")
+else:
+    print("Condition false")
+
+if name == "abc" and age == 56:
+    print("Condition True")
+else:
+    print("Condition false")
+
+# Or operator- Return True If any one of the all condition is satisfied.
+if name == "abc" or age == 19:
+    print("Condition True")
+else:
+    print("Condition false")
+
+if name == "adc" or age == 19:
+    print("Condition True")
+else:
+    print("Condition false")
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
