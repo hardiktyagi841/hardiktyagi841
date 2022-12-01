ea8c9c58ad36245f83607cacce260c82170dadca
hardiktyagi841 <119604493+hardiktyagi841@users.noreply.github.com>
2022-12-01 20:03:18 +0530

    Create Variables-CipherSchools.py

---
+++ Variables-CipherSchools.py
@@ -0,0 +1,34 @@
+num1=2
+print(num1)
+num1=4
+print(num1)
+
+# we can store string, number and characters.
+
+name="hardik"
+print(name)
+name="Harshit"
+print(name) # This shows python is a dynamic language.
+
+# Rules-
+
+# first letter should be a letter or _ and not be a number or character.
+# 1number = 3 (wrong variable type)
+
+name="hardik"
+_name="hardik"
+
+user_one_name = "Rohit"  # Snake case writing
+UserOneName = "Mohit"  # Camel case writing
+
+# we cann't use space between words used in name of a variable.
+
+#         Assigning values to variables-
+
+# Different values to multiple variables-
+name,age = "hardik",19
+print("Hello " + name + " your age is " + str(age))
+
+# Same value to multiple variables-
+x=y=z=3
+print(x+y+z)
